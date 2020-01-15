73d53d4e2b6cf9411e3683f8984c57daae8da780
Rafiology <rafidegusto@gmail.com>
2020-01-15 23:28:05 +0100

    2

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
-var Request = new XMLHttpRequest();
-Request.open('GET','js/data.json')
-Request.onload = function () {
-    console.log(Request.responseText);
-}
+var container = document.querySelector(".json-containter");
+var buttonClick = document.querySelectorAll(".btn");
+var number = 0;
 
-Request.send();
-
-function init() {
-    loadJSON(function(response) {
-      // Parse JSON string into object
-      var actual_JSON = JSON.parse(response);
-      console.log(actual_JSON);
+buttonClick.forEach(function(buttonClick) {
+    buttonClick.addEventListener("click",  function() {
+        var Request = new XMLHttpRequest();
+        Request.open('GET','https://rafiology.github.io/js/data.json')
+        Request.onload = function () {
+        var Data = JSON.parse(Request.responseText);   
+        console.log(Data[number]);
+        
+        container.innerHTML = "<h1>" + Data[number].kraj + "</h1>" +
+            "<p>" + Data[number].opis + "</p>";
+        for (var i = 0; i < Data[number].potrawy.length; i++) {
+            container.innerHTML += "<li>" + Data[number].potrawy[i] + "</li>";
+            }
+        };
+        Request.send();
     });
-  }
+});
+
